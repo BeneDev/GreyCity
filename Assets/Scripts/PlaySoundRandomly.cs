2a501a764f6d85990cf50304a81c91646de8376f
Bene_Dev <seriousbenedikt@web.de>
2018-03-16 14:24:01 +0100

    Ambient sounds
    Always playing wind and randomly thunder

---
+++ Assets/Scripts/PlaySoundRandomly.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundRandomly : MonoBehaviour {
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] float maxTimeInBetween = 30f;
+    [SerializeField] float minTimeInBetween = 1f;
+
+
+    private float counter;
+	
+	// Update is called once per frame
+	void Update () {
+		if(counter <= 0f)
+        {
+            audioSource.volume = Random.Range(0.2f, 1f);
+            audioSource.Play();
+            counter = Random.Range(minTimeInBetween, maxTimeInBetween);
+        }
+        else if(counter > 0f)
+        {
+            counter -= Time.deltaTime;
+        }
+	}
+}
