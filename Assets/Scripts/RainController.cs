f9432d33cfa8bb987e0b5a77c441ee833f0f56e1
BeneDev <33699887+BeneDev@users.noreply.github.com>
2018-03-21 12:50:27 +0100

    Right background is used

---
+++ Assets/Scripts/RainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour {
+
+    [SerializeField] GameObject rainObject;
+    Camera cam;
+    [SerializeField] float normalCamZoom = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rainObject)
+        {
+            if(cam.orthographicSize != normalCamZoom)
+            {
+                rainObject.SetActive(false);
+            }
+            else
+            {
+                //rainObject.SetActive(true);
+            }
+        }
+	}
+}
