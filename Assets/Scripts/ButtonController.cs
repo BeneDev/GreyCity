58c779d08e5244e2f50de4834dd383a000aee9ce
BeneDev <33699887+BeneDev@users.noreply.github.com>
2018-03-22 12:08:53 +0100

    More level design

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,6 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour {
 
+    [SerializeField] GameObject fadeInObject;
+
+    private void OnEnable()
+    {
+        StartCoroutine(FadeIn(2f));
+        
+    }
+
+    IEnumerator FadeIn(float duration)
+    {
+        Color fadeColor = fadeInObject.GetComponent<SpriteRenderer>().color;
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            fadeColor.a = 255 * t / duration;
+            fadeInObject.GetComponent<SpriteRenderer>().color = fadeColor;
+            yield return new WaitForEndOfFrame();
+        }
+        fadeColor.a = 255;
+        fadeInObject.GetComponent<SpriteRenderer>().color = fadeColor;
+    }
+
     public void OnPlayAgain()
     {
         SceneManager.LoadScene(1);
